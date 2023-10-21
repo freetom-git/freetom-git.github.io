ca11c080c90bd7017680f48447b225e91be800d2
freetom <freetom+git@protonmail.com>
2023-10-21 18:45:40 +0200

    initial commit

---
+++ config.js
@@ -0,0 +1,5 @@
+window.__CONFIG__ = {
+  // url must NOT end with a slash
+  VITE_CORS_PROXY_URL: "https://simple-proxy.cloudflare-defraud239.workers.dev",
+  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwZjY1ZjhjYTMyMGYxM2U2OTU5NWFmOTYyZmQyYzAyZCIsInN1YiI6IjY1MzNmY2M3OGNmY2M3MDE0YzhkNjFjNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.5XB8_Z1jOBNPiR3zXThcqd2WIpEc8XmczuCDgzjw93M"
+};
